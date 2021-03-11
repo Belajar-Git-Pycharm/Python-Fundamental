209fa847ddffab4bb0aa8fa2fae2b0139591a5cd
Mohammad Andrie <andrie.wibisono354@gmail.com>
2021-03-12 00:05:05 +0700

    merubah file bebas aja dari pycharm ke github

---
+++ main.py
@@ -0,0 +1,9 @@
+print('hello world!')
+
+
+# perhitungan luas persegi panjang
+p = 10
+l = 12
+luas_persegipanjang = p * l
+
+print (f'luas persegi panjang dengan panjang = {p}, lebar = {l}, adalah {luas_persegipanjang}')
