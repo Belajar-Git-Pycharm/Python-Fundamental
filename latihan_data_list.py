e429301b89782790f8d0727bc5451f3b7d4b017b
Mohammad Andrie <andrie.wibisono354@gmail.com>
2021-04-04 14:55:46 +0700

    penambahan latihan pembuatan data list

---
+++ latihan_data_list.py
@@ -0,0 +1,10 @@
+mobil = []
+mobil.append('honda')
+mobil.append('toyota')
+mobil.append('suzuki')
+mobil.append('nissan')
+mobil.append('mercedes-benz')
+
+print(mobil)
+
+print(f'\nsaya ingin pakai mobil : {mobil [4]}')
