b6d69bb39e51b80d87d808719353ed24fbd425f8
Mohammad Andrie <andrie.wibisono354@gmail.com>
2021-04-06 23:20:56 +0700

    belajar tipe data dictionary

--- data_dictionary.py
+++ data_dictionary.py
@@ -18,3 +18,18 @@ print(kamus['mother'])
 
 # eksekusi searah (dari kiri ke kanan), tidak bisa sebaliknya (dari kanan ke kiri)
 
+data_dari_server_gojek = {
+    'tanggal' : '2021-04-06',
+    'driver_list' : [
+        {'nama': 'Eko', 'jarak' : 10},
+        {'nama': 'Dwi', 'jarak' : 30},
+        {'nama': 'Tri', 'jarak' : 100},
+        {'nama': 'Catur', 'jarak' : 1000}
+    ]
+}
+print(data_dari_server_gojek)
+print(f"\nDriver di sekitar sini {data_dari_server_gojek['driver_list']}")
+print(f"Driver #1 {data_dari_server_gojek['driver_list'][0]}")
+print(f"Driver #2 {data_dari_server_gojek['driver_list'][1]}")
+print(f"Driver #3 {data_dari_server_gojek['driver_list'][2]}")
+print(f"Driver #4 {data_dari_server_gojek['driver_list'][3]}")
